16c0b69cfcee7ba0716ef5d71095ed0bd7d9107d
verixx <abdurraqeeb53@gmail.com>
2017-10-14 21:04:21 +1100

    Update testing.py

--- cogs/testing.py
+++ cogs/testing.py
@@ -1,13 +1,1 @@
-import discord
-from discord.ext import commands
 
-class Testing:
-    def __init__(self, bot):
-        self.bot = bot
-       
-    @commands.command()
-    async def hi(self, ctx):
-        await ctx.send('hello!')
-        
-def setup(bot):
-	return bot.add_cog(Testing(bot))
